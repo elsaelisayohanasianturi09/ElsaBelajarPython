d800d292ed794aed153fa3aaa33bb5642c03ee7f
elsaelisayohanasianturi09 <elsaelisayohana05@gmail.com>
2024-01-15 23:27:29 +0700

    chgn1

---
+++ belajarpythonOOP.py
@@ -0,0 +1,22 @@
+class Food: #Template
+    pass
+food1 = Food()
+food2 = Food()
+food3 = Food()
+
+food1.name = "Bakso"
+food1.price  = 12000
+food1.stock = 30
+
+food2.name = "Mie Ayam"
+food2.price = 15000
+food2.stock = 39
+
+food3.name = "Soup"
+food3.price = 20000
+food3.stock = 30
+
+print(food1)
+print(food1.name)
+print(food2.price)
+print(food3.name)
